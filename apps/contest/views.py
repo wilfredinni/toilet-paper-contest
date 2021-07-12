3b41ca288035a397f62551627ccca5f3ad691bc1
wilfredinni <carlos.w.montecinos@gmail.com>
2021-07-12 00:06:53 -0400

    optimize random user

--- apps/contest/views.py
+++ apps/contest/views.py
@@ -1,4 +1,5 @@
 import random
+from django.db.models import Max
 
 from rest_framework import viewsets
 from rest_framework.permissions import IsAdminUser
@@ -13,13 +14,12 @@ class ContestWinnerViewset(viewsets.ModelViewSet):
     http_method_names = ["get"]
 
     def get_queryset(self):
-        # exclude staff and non active users
-        queryset = CustomUser.objects.filter(
-            is_active=True, is_superuser=False, is_staff=False
-        )
-
-        # more performant than .order_by("?")[:1]
-        if queryset:
-            return [random.choice(queryset)]
-
-        return queryset
+        # seems to be the more performant solution
+        # https://books.agiliq.com/projects/django-orm-cookbook/en/latest/random.html
+        # ugly but effective !!!
+        max_id = CustomUser.objects.aggregate(max_id=Max("id"))["max_id"]
+        while True:
+            pk = random.randint(1, max_id)
+            user = CustomUser.objects.filter(pk=pk).first()
+            if user and user.is_active and not user.is_staff and not user.is_superuser:
+                return [user]
